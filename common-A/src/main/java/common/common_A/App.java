6db5ac0f5205e8d4203c61291f88aa50abd1b629
Lin <bakerybread@hotmail.com>
2020-03-24 16:01:26 +0800

    [TING] FIRST PUSH

---
+++ common-A/src/main/java/common/common_A/App.java
@@ -0,0 +1,13 @@
+package common.common_A;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
